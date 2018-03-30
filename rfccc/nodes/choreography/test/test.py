0bd52270391d32bdb79860bfb1d05ebe8d98164c
Damien Zufferey <zufferey@mpi-sws.org>
2018-03-30 14:58:21 +0200

    predicate in spec

--- rfccc/nodes/choreography/test/test.py
+++ rfccc/nodes/choreography/test/test.py
@@ -8,23 +8,24 @@ import unittest
 
 
 # The example from the paper
+# origin is (0,0), target is (2,0)
 def cartAndArmFetch():
     return ''' G =
         def x0 = C -> A : action(fold) ; x1
             x1 = (C : idle(), A : fold()) ; x2
             x2 = A -> C : state(folded) ; x3
             x3 + x6 = x4
-            x4 = [!inReach(target)]x5 + [inReach(target)]x7
-            x5 = ( C : moveToward(target), A : idle()) ; x6
+            x4 = [sqrt((C_x - 2)**2 + (C_y - 0)**2) > 0.1] x5 + [sqrt((C_x - 2)**2 + (C_y - 0)**2) <= 0.1] x7
+            x5 = (C : moveToward(0,0, 2,0), A : idle()) ; x6
             x7 = C -> A : action(grab) ; x8
             x8 = ( C : idle(), A : grab(target)) ; x9
             x9 = A -> C : state(grabbed) ; x10
             x10 = C -> A : action(fold) ; x11
-            x11 = ( C : idle(), A : fold()) ; x12
+            x11 = (C : idle(), A : fold()) ; x12
             x12 = A -> C : state(folded) ; x13
             x13 + x16 = x14
-            x14 = [!inReach(target)]x15 + [inReach(target)]x17
-            x15 = ( C : moveToward(home), A : idle()) ; x16
+            x14 = [sqrt((C_x - 0)**2 + (C_y - 0)**2) > 0.1] x15 + [sqrt((C_x - 0)**2 + (C_y - 0)**2) <= 0.1] x17
+            x15 = (C : moveFromTo(2, 0, 0, 0), A : idle()) ; x16
             x17 = C -> A : state(done) ; x18
             x18 = end
         in [true]x0
